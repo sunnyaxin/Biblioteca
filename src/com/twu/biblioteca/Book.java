ed7885a53af4e1ee7d83fb939078213d8ced1537
Yaxin Wang <yxWang@CNyxWang.local>
2017-07-28 15:26:16 +0800

    Add Book, add book list.

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+public class Book {
+    private String name, author, year;
+
+    public Book(String name, String author, String year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+}
